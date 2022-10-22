3e111018de41ef89da01b3025a5c11dbb33837bd
Kanchana Chathuranga <73539093+kanchanachathuranga@users.noreply.github.com>
2022-10-22 09:18:21 +0530

    complete the issue
    Adding two numbers
    Subtracting two digits
    Multiplying two numbers
    Dividing two numbers
    Generate a grade per mark

---
+++ Dividing two numbers/kanchana.py
@@ -0,0 +1,4 @@
+n1=int(input("Enter fest number"))
+n2=int(input("Enter second number"))
+total=n1/n2
+print("Total = ",total)
---
+++ Generate a grade per marks/kanchana.py
@@ -0,0 +1,11 @@
+marks=int(input("Enter marks :-"))
+if 100>=marks>=75:
+    print("Grade A")
+elif 75>marks>=65:
+    print("Grade B")
+elif 65>marks>=55:
+    print("Grade C")
+elif 55>marks>=35:
+    print("Grade S")
+elif 35>marks>=0:
+    print("Grade F")
